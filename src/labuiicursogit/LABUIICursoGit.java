d187b4e6e8447daa76e20d6e3018fd092d62d006
alopezorozcouaz <147572356+alopezorozcouaz@users.noreply.github.com>
2026-02-24 11:49:09 -0600

    Cambiar texto a JavaScript

--- src/labuiicursogit/LABUIICursoGit.java
+++ src/labuiicursogit/LABUIICursoGit.java
@@ -14,7 +14,7 @@ public class LABUIICursoGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("¡Hola freeCodeCamp!");
+        System.out.println("¡Hola JavaScript!");
     }
     
 }
